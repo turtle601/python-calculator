6a38a22cfd327cd4c0ee59b40af69017e2d8f5ab
turtle601 <turtle601@naver.com>
2022-11-29 09:35:46 +0900

    feat: 숫자 입력 표시 수정 [#4]

--- calculator_main.py
+++ calculator_main.py
@@ -16,14 +16,10 @@ class Main(QDialog):
         layout_equation_solution = QFormLayout()
 
         ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
-        label_equation = QLabel("Equation: ")
-        label_solution = QLabel("Number: ")
-        self.equation = QLineEdit("")
-        self.solution = QLineEdit("")
+        self.calculatedNum = QLineEdit("")
 
         ### layout_equation_solution 레이아웃에 수식, 답 위젯을 추가
-        layout_equation_solution.addRow(label_equation, self.equation)
-        layout_equation_solution.addRow(label_solution, self.solution)
+        layout_equation_solution.addRow(self.calculatedNum)
 
         ### 사칙연상 버튼 생성
         button_plus = QPushButton("+")
