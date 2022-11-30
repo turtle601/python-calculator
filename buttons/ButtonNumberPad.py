556e99f1e7e5b946503cd4d582fa444f5d5902d3
turtle601 <turtle601@naver.com>
2022-11-30 09:32:50 +0900

    feat: 숫자 입력 시 화면에 보여주는 기능 구현 [#09]

--- buttons/ButtonNumberPad.py
+++ buttons/ButtonNumberPad.py
@@ -5,11 +5,18 @@ class ButtonNumberPad(QDialog):
         super().__init__()
 
         self.layout = props['layout']
+        self.getState = props['getState']
+        self.setState = props['setState']
+        self.rerender = props['rerender']
+        
         self.number_button_dict = {}
 
     def template(self):
         for number in range(0, 10):
             self.number_button_dict[number] = QPushButton(str(number))
+
+        self.dotBtn = QPushButton('.')
+        self.doubleZeroBtn = QPushButton('00')
 
     def numPadOverZero(self, number, button):
         share, rest = divmod(number - 1, 3)
@@ -19,10 +26,10 @@ class ButtonNumberPad(QDialog):
         self.layout.addWidget(button, 6, 1)
     
     def numPadDot(self):
-        self.layout.addWidget(QPushButton('.'), 6, 2)
+        self.layout.addWidget(self.dotBtn, 6, 2)
     
     def numPadDoubleZero(self):
-        self.layout.addWidget(QPushButton('00'), 6, 0)
+        self.layout.addWidget(self.doubleZeroBtn, 6, 0)
 
     def render(self):
         self.template()
@@ -32,3 +39,19 @@ class ButtonNumberPad(QDialog):
 
         self.numPadDot()
         self.numPadDoubleZero()
+
+        self.setEvent()
+
+    def setEvent(self):
+        for number in range(0, 10):
+            self.number_button_dict[number].clicked.connect(lambda state, num = number: self.clickNumPad(num))
+
+        self.dotBtn.clicked.connect(lambda: self.clickNumPad('.'))
+        self.doubleZeroBtn.clicked.connect(lambda: self.clickNumPad('00'))
+    
+
+    def clickNumPad(self, number):
+        newData = self.getState() + str(number)
+
+        self.setState(newData)
+        self.rerender()
